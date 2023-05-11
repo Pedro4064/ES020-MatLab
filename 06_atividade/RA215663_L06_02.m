b4d15fe5d774276d6721b484fef2816c8d9d4060
Pedro4064 <pedrohlcruz@gmail.com>
2023-05-11 16:20:27 -0300

    [FIXED] Parentheses location issue

--- 06_atividade/RA215663_L06_02.m
+++ 06_atividade/RA215663_L06_02.m
@@ -1,18 +1,18 @@
 function [x1,x2] = RA215663_L06_02()
     clc, clear;
     
-    ka = 9e2;
-    kb = 2e2;
+    ka = 9;
+    kb = 2;
 
-    La = 10e-2;
-    Lb = 10e-2;
+    La = 10;
+    Lb = 10;
 
     F1 = 2;
     F2 = 4;
 
-    U = @(X) ((1/2) .* ka .* sqrt(X(1).^2 + (La - X(2)).^2) - La).^2 + ((1/2) .* kb .* sqrt(X(1).^2 + (Lb + X(2)).^2) - Lb).^2 -F1.*X(1) -F2.*X(2);
+    U = @(X) (1/2) .* ka .* (sqrt(X(1).^2 + (La - X(2)).^2) - La).^2 + ((1/2) .* kb .* sqrt(X(1).^2 + (Lb + X(2)).^2) - Lb).^2 -F1.*X(1) -F2.*X(2);
 
-    result = fminsearch(U, [1 1]);
+    result = fminsearch(U, [-0.5; 0.5]);
     x1 = result(1);
     x2 = result(2);
 
