6ea65e9f5aa12828b164b0799c2be0511670ec69
Pedro4064 <pedrohlcruz@gmail.com>
2023-05-21 22:11:48 -0300

    [FIXED] Issues regarding unit conversion

--- Trabalho_01/aux/sing_function.m
+++ Trabalho_01/aux/sing_function.m
@@ -1,10 +1,10 @@
-function [f_x] = sing_function(x, f, n)
+function [f_x] = sing_function(f,n)
 % SING_FUNCTION Returs the resulting values for the Singularity function
-% <f>^n over the x domain.
+% <f>^n, which is zero for f<0 and f^2 otherwise.
 %
-% [f_x] = SIGN_FUNCTION(0:10, @(x) x - L./4, 2) Returns, over the x vector,
+% [f_x] = SIGN_FUNCTION(X - L./4, 2) Returns, over the X vector,
 % the result of the singularity function <x - L/4>^2, which is zero for 
 % x< L/4 and (x - L/4)^2 for x>= L/4
 
-    f_x = (f(x) >= 0) .* (f(x) .^ n); % If f(x) greater than 0, y = f(x)^n, else y = 0
+    f_x = (f >= 0) .* (f .^ n); % If f(x) greater than 0, y = f(x)^n, else y = 0
 end
